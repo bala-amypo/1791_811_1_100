b46b02bdd25a0f191c4c1ac620bc147601eacddb
bala-amypo <bala@amypo.in>
2025-12-23 07:56:23 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,11 +1,10 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
-
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,19 +12,8 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Vendor Compliance Document Validator API")
-                        .version("1.0.0")
-                        .description("API documentation for Vendor Compliance Document Validator")
-                        .contact(new Contact()
-                                .name("Your Name")
-                                .email("your.email@example.com")))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
-    }
+                .servers(List.of(
+                        new Server().url("https://9361.408procr.amypo.ai/")
+                ));
+        }
 }
