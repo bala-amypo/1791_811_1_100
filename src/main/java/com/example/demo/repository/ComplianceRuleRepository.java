67baff3b65fdb80d69d0a88b02587e3376c45f70
bala-amypo <bala@amypo.in>
2025-12-26 04:53:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/ComplianceRuleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.ComplianceRule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComplianceRuleRepository extends JpaRepository<ComplianceRule, Long> {
-}
--- src/main/java/com/example/demo/repository/ComplianceScoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.ComplianceScore;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComplianceScoreRepository extends JpaRepository<ComplianceScore, Long> {
-}
