99ef90bd1e60824ce3b02df726e22e60dfde6d82
bala-amypo <bala@amypo.in>
2025-12-27 06:53:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/VendorDocumentRepository.java
+++ src/main/java/com/example/demo/repository/VendorDocumentRepository.java
@@ -1,14 +1,10 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.DocumentType;
+import com.example.demo.model.VendorDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface DocumentTypeRepository extends JpaRepository<DocumentType, Long> {
-
-    // This will return all DocumentType entities where required = true
-    List<DocumentType> findByRequiredTrue();
+public interface VendorDocumentRepository extends JpaRepository<VendorDocument, Long> {
+    // add custom methods if needed
 }
