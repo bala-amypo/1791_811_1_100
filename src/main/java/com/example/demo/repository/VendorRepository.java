62596b263530857253f22bf2b8e1a5f4023bca3b
bala-amypo <bala@amypo.in>
2025-12-27 07:35:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/VendorRepository.java
+++ src/main/java/com/example/demo/repository/VendorRepository.java
@@ -1,9 +1,7 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import com.example.demo.model.Vendor;
 
-@Repository
 public interface VendorRepository extends JpaRepository<Vendor, Long> {
 }
