47bca441550e7cf0a32eda67dd562ecdba21e072
bala-amypo <bala@amypo.in>
2025-12-28 10:47:54 +0000

    User commit

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
